f7dd61d9a87803b7c567b8a5d1a9248c224ddd45
Yogesh <ypandey041@gmail.com>
2026-07-29 15:44:49 +0530

    morgan

--- fullstack/app.js
+++ fullstack/app.js
@@ -2,14 +2,30 @@ let express = require("express");
 let app = express();
 let dotenv = require('dotenv');
 dotenv.config()
+let morgan = require('morgan');
+let fs = require('fs');
 let port = process.env.PORT || 6700;
 let categoryRouter = require('./src/controller/CategoryRouter');
 let productRouter = require('./src/controller/ProductRouter');
 
 
 
+//middleware
+app.use(morgan('common',{stream:fs.createWriteStream('./app.log')}))
+
+
+//static file path
+app.use(express.static(__dirname+'/public'))
+//html file path
+app.set('views', './src/views')
+//view engine
+app.set('view engine', 'ejs')
+
+//default
 app.get('/', (req, res) => {
-    res.send('Hi from Express');
+    //res.send('Hi from Express');
+
+    res.render('index')
 })
 
 
